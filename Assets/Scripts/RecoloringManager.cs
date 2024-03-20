553b5f43f8cb7f42f8e4a152206a7ccb14d642d5
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-03-20 22:08:15 +0300

    feat

--- Assets/Scripts/RecoloringManager.cs
+++ Assets/Scripts/RecoloringManager.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class RecoloringManager : MonoBehaviour
 {
+    private float _currentTime;
     private Color _startColor;
+    private Color _finalColor;
 
     private Renderer _renderer;
     // Start is called before the first frame update
     void Start()
     {
         _renderer = GetComponent<Renderer>();
-        _startColor = Random.ColorHSV(0.15f,0.5f,0.8f,1,0.8f,1);
+        _startColor = GenerateNewColor();
         _renderer.material.color = _startColor;
+        _finalColor = GenerateNewColor();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _currentTime += Time.deltaTime;
+        _renderer.material.color = Color.Lerp(_startColor, _finalColor, _currentTime);
+    }
+
+    private Color GenerateNewColor()
+    {
+        return Random.ColorHSV(0, 1, 1, 1, 0.5f, 0.9f);
     }
 }
